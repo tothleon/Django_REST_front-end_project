2953104b8c2507f06689ef33dc226023dbe41c84
tothleon <toth.leon.999@gmail.com>
2022-09-28 22:35:22 +0200

    Basic Django back end finished

--- api/views.py
+++ api/views.py
@@ -21,6 +21,39 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def taskList(request):
-	tasks = Task.objects.all().order_by('-id')
+	tasks = Task.objects.all()
 	serializer = TaskSerializer(tasks, many=True)
 	return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	tasks = Task.objects.get(id=pk)
+	serializer = TaskSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['GET','POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['GET','DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    task.delete()
+    
+    return Response('Item succsesfully delete!')
