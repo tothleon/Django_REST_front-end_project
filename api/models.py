553ecc15c57d68df0b66bf9909900195f856ae6d
tothleon <toth.leon.999@gmail.com>
2022-09-28 21:15:50 +0200

    added api/task-list/

--- api/models.py
+++ api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
-
+      
     def __str__(self):
-        return self.title
+      return self.title
